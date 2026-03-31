841ff766ceda51847f938a6320744c6e0abca04d
HarperMiller7519934 <hlm7519934@lpssonline.com>
2026-03-31 17:55:40 +0000

    todo 2 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -21,7 +21,7 @@ var init = function (window) {
         
         // TODO 1 : Declare and initialize our variables
 var circle
-var circle = []
+var circles = []
 
         // TODO 2 : Create a function that draws a circle 
         function drawCircle(){
@@ -33,9 +33,11 @@ var circle = []
 
 
         // TODO 3 : Call the drawCircle() function
-
-
-
+drawCircle()
+drawCircle()
+drawCircle()
+drawCircle()
+drawCircle()
         // TODO 7 : Use a loop to create multiple circles
 
 
